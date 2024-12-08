273f88ba8e45c5d963a05786b3612bc109da9bf8
ajay91py <166129570+ajay91py@users.noreply.github.com>
2024-12-08 18:18:58 +0530

    made somewhat functional app, need to add more cosmetics and more utilities

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const addABook=document.getElementById('add-newbook')
 const okbtn=document.getElementById('okbtn')
 const clsbtn=document.getElementById('close-button')
+const formData=document.getElementById('book-details')
 let dialog=document.getElementById('hidden')
 const container=document.getElementById('container')
 
 const library=[]
-
-function book(name, author, pages, status) {
-    this.name=name;
+//obj constructor
+function book(title, author, pages, status) {
+    this.title=title;
     this.pages=pages;
     this.author=author;
     this.status=status;
     this.info=function() {
-      return `${name} by ${author}, ${pages} pages, ${status}`
+      return `${title} by ${author}, ${pages} pages, ${status}`
     }
   }
-
+//add books to library here
   function addBooks() {
     let title=document.getElementById('title').value
     let author=document.getElementById('author').value
@@ -24,19 +25,32 @@ function book(name, author, pages, status) {
     let Book= new book(title, author, pages,status)
     library.push(Book)
     console.log(library);
-    container.innerHTML=populateDisplay()
-
+    populateDisplay({title,author,pages,status})
+    formData.reset()
   }
 
   addABook.addEventListener('click', ()=>{
+    
     dialog.showModal();
 
   })
 
-  okbtn.addEventListener('click', ()=>{addBooks()})
+  okbtn.addEventListener('click', ()=>{addBooks();
+    
+  })
 clsbtn.addEventListener('click', ()=>dialog.close())
 
-function populateDisplay() {
-  library.forEach()
-  return ``
+function populateDisplay({title,author,pages,status}) {
+
+  container.innerHTML=""
+  library.forEach(({title,author,pages,status})=>{
+    let card=document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML=`<p>Title:${title}</p>
+    <p>Author:${author}</p>
+    <p>Pages:${pages}</p>
+    `
+    container.append(card) 
+  })
+  
 }
